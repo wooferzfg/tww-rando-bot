6b2d5dbec83ffefb09e460b9c2e06d501cad2f0e
wooferzfg <spapushin@gmail.com>
2020-10-11 15:54:26 -0500

    Don't use chdir

--- randobot/generator.py
+++ randobot/generator.py
@@ -13,8 +13,7 @@ class Generator:
         seed_name = uuid.uuid4()
         file_name = "".join(random.choice(string.ascii_uppercase) for _ in range(6))
 
-        os.chdir("wwrando")
-        os.system(f"py wwrando.py -seed={seed_name} -permalink=MS44LjAATmljZUFjY29tbW9kYXRpbmdLb3JvawAXAwYCDxDADAAAAAAAAAA=")
+        os.system(f"py wwrando/wwrando.py -seed={seed_name} -permalink=MS44LjAATmljZUFjY29tbW9kYXRpbmdLb3JvawAXAwYCDxDADAAAAAAAAAA=")
 
         permalink_file_name = f"permalink_{seed_name}.txt"
         permalink_file = open(permalink_file_name, "r")
