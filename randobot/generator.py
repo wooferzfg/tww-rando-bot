5d8b5e6f53363b7c8952b1ecc8c4b3a314675c1d
wooferzfg <spapushin@gmail.com>
2020-12-22 22:54:36 -0600

    Fix formatting

--- randobot/generator.py
+++ randobot/generator.py
@@ -12,7 +12,9 @@ class Generator:
     def generate_seed(self):
         seed_name = uuid.uuid4()
         file_name = "".join(random.choice(string.digits) for _ in range(6))
+
         os.system(f"python wwrando/wwrando.py -seed={seed_name} -permalink=MS44LjAATmljZUFjY29tbW9kYXRpbmdLb3JvawAXAwYCDxDADAAAAAAAAAA=")
+
         permalink_file_name = f"permalink_{seed_name}.txt"
         permalink_file = open(permalink_file_name, "r")
         permalink = permalink_file.read()
