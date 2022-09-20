bc771e98a60f1e5d490aab6c117baad1e2b46a1c
wooferzfg <spapushin@gmail.com>
2022-09-20 12:05:46 -0400

    Add debug messages

--- randobot/handler.py
+++ randobot/handler.py
@@ -98,6 +98,13 @@ class RandoHandler(RaceHandler):
                         await self.force_start()
                         self.state["spoiler_log_race_started"] = True
 
+                if self.state.get("breaks_set"):
+                    await self.send_message("self.data.get('started_at'):")
+                    if self.data.get("started_at") is None:
+                        await self.send_message("None")
+                    else:
+                        await self.send_message(self.data.get("started_at"))
+
                 if self.data.get("started_at") is not None and self.state.get("breaks_set"):
                     break_duration = self.state.get("break_duration")
                     break_interval = self.state.get("break_interval")
@@ -105,8 +112,15 @@ class RandoHandler(RaceHandler):
                     if self.state.get("last_break_time") is None:
                         self.state["last_break_time"] = isodate.parse_date(self.data.get("started_at"))
 
+                    await self.send_message("parsed date")
+
                     seconds_since_last_break = (datetime.now() - self.state.get("last_break_time")).total_seconds()
+
+                    await self.send_message(f"second_since_last_break: {str(seconds_since_last_break)}")
+
                     seconds_until_next_break = (break_interval * 60) - seconds_since_last_break
+
+                    await self.send_message(f"seconds_until_next_break: {str(seconds_since_last_break)}")
 
                     if not self.state.get("break_warning_sent") and seconds_until_next_break < 300:
                         await self.send_message("@entrants Reminder: Next break in 5 minutes.")
