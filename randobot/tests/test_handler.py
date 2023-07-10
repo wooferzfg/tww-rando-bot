fb9f25b3cc6a90a83df854316849ffdd9b7710f1
wooferzfg <spapushin@gmail.com>
2023-07-10 18:21:22 -0400

    WIP

---
+++ randobot/tests/test_handler.py
@@ -0,0 +1,12 @@
+import unittest
+
+from randobot.handler import RaceHandler
+
+
+class TestHandler(unittest.TestCase):
+    def test_ra_command(self):
+        print("Hello world!")
+
+
+if __name__ == '__main__':
+    unittest.main()
