cd469f26dbfe44c1ff4a7aaea648f377ea9b9c43
wooferzfg <spapushin@gmail.com>
2023-07-10 19:01:44 -0400

    Add test with RA

--- randobot/tests/test_handler.py
+++ randobot/tests/test_handler.py
@@ -83,6 +83,34 @@ class TestHandler(unittest.IsolatedAsyncioTestCase):
             call("wwrando", "MS4xMC4wAEEAFwMiAHPowgMMsACCcQ8AAMkHAAAA", "test_user", False)
         ])
 
+    @patch.object(MockGenerator, "generate_seed", side_effect=mock_generate_seed_standard)
+    @patch.object(RandoHandler, "set_raceinfo", return_value=async_return(None))
+    @patch.object(RandoHandler, "send_message", return_value=async_return(None))
+    async def test_rollseed_s6_with_ra(self, mock_send_message, mock_set_raceinfo, mock_generate_seed):
+        generator = MockGenerator()
+        state = {}
+        handler = create_rando_handler(generator, state)
+        await handler.ex_rollseed(["s6+4drm+nosword"], get_mock_message_data())
+
+        self.assertEqual(mock_send_message.call_count, 4)
+        mock_send_message.assert_has_calls([
+            call("Rolling seed..."),
+            call("Settings: s6+4drm+nosword"),
+            call("Permalink: PERMA_MS4xMC4wAEEAFwMiAHMKwwMc8ACCcQ8AAMkHAAAA"),
+            call("Seed Hash: SEED HASH"),
+        ])
+
+        self.assertEqual(mock_set_raceinfo.call_count, 2)
+        mock_set_raceinfo.assert_has_calls([
+            call("Settings: s6+4drm+nosword", False, False),
+            call("PERMA_MS4xMC4wAEEAFwMiAHMKwwMc8ACCcQ8AAMkHAAAA | Seed Hash: SEED HASH", False, False)
+        ])
+
+        self.assertEqual(mock_generate_seed.call_count, 1)
+        mock_generate_seed.assert_has_calls([
+            call("wwrando", "MS4xMC4wAEEAFwMiAHMKwwMc8ACCcQ8AAMkHAAAA", "test_user", False)
+        ])
+
 
 if __name__ == "__main__":
     unittest.main()
