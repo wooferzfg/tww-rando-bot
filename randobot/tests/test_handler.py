dfae493f965c3229997118ace97e0673ca297f09
wooferzfg <spapushin@gmail.com>
2023-07-10 18:58:50 -0400

    Add S6 test

--- randobot/tests/test_handler.py
+++ randobot/tests/test_handler.py
@@ -10,6 +10,20 @@ class MockGenerator():
         raise Exception("Method not properly mocked")
 
 
+def mock_generate_seed_standard(randomizer_path, permalink, username, generate_spoiler_log):
+    return {
+        "file_name": "FILENAME",
+        "permalink": f"PERMA_{permalink}",
+        "seed_hash": "SEED HASH",
+        "spoiler_log_url": None,
+    }
+
+
+class MockLogger():
+    def info(self, data):
+        pass
+
+
 def async_return(result):
     f = asyncio.Future()
     f.set_result(result)
@@ -17,7 +31,9 @@ def async_return(result):
 
 
 def create_rando_handler(generator, state):
-    return RandoHandler(generator, logger=None, conn=None, state=state)
+    handler = RandoHandler(generator, logger=MockLogger(), conn=None, state=state)
+    handler.room_setup()
+    return handler
 
 
 def get_mock_message_data():
@@ -41,6 +57,32 @@ class TestHandler(unittest.IsolatedAsyncioTestCase):
             )
         ])
 
+    @patch.object(MockGenerator, "generate_seed", side_effect=mock_generate_seed_standard)
+    @patch.object(RandoHandler, "set_raceinfo", return_value=async_return(None))
+    @patch.object(RandoHandler, "send_message", return_value=async_return(None))
+    async def test_rollseed_s6(self, mock_send_message, mock_set_raceinfo, mock_generate_seed):
+        generator = MockGenerator()
+        state = {}
+        handler = create_rando_handler(generator, state)
+        await handler.ex_rollseed(["s6"], get_mock_message_data())
 
-if __name__ == '__main__':
+        self.assertEqual(mock_send_message.call_count, 3)
+        mock_send_message.assert_has_calls([
+            call("Rolling seed..."),
+            call("Permalink: PERMA_MS4xMC4wAEEAFwMiAHPowgMMsACCcQ8AAMkHAAAA"),
+            call("Seed Hash: SEED HASH"),
+        ])
+
+        self.assertEqual(mock_set_raceinfo.call_count, 1)
+        mock_set_raceinfo.assert_has_calls([
+            call("PERMA_MS4xMC4wAEEAFwMiAHPowgMMsACCcQ8AAMkHAAAA | Seed Hash: SEED HASH", False, False)
+        ])
+
+        self.assertEqual(mock_generate_seed.call_count, 1)
+        mock_generate_seed.assert_has_calls([
+            call("wwrando", "MS4xMC4wAEEAFwMiAHPowgMMsACCcQ8AAMkHAAAA", "test_user", False)
+        ])
+
+
+if __name__ == "__main__":
     unittest.main()
