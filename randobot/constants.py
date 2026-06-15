40d119b82a32a64265210dccd62e4237431372dc
wooferzfg <spapushin@gmail.com>
2026-06-15 11:14:42 -0400

    Update DEV_TRACKER URL for s9 tournament

--- randobot/constants.py
+++ randobot/constants.py
@@ -64,7 +64,7 @@ DEV_SL_PERMALINKS = OrderedDict([
 DEV_SL_DEFAULT = ["s9"]
 
 DEV_DOWNLOAD = "https://github.com/tanjo3/wwrando/releases/tag/dev_tanjo3.1.10.7.3"
-DEV_TRACKER = "https://wooferzfg.me/tww-rando-tracker/wwrando-dev-tanjo3"
+DEV_TRACKER = "https://wooferzfg.me/tww-rando-tracker/s9-tournament"
 MINIBLINS_TRACKER = "https://wooferzfg.me/tww-rando-tracker/miniblins"
 
 RS_VERSION = "RS1.4.0-dev3"
