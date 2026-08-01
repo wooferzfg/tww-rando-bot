a267f68f05b90101651413354107406b82ae8234
wooferzfg <spapushin@gmail.com>
2026-08-01 12:37:12 -0400

    Remove miniblins tracker and add s9 tournament tracker link (#137)

--- randobot/tests/test_handler.py
+++ randobot/tests/test_handler.py
@@ -261,6 +261,41 @@ class TestHandler(unittest.IsolatedAsyncioTestCase):
             call("Seed Hash: SEED HASH"),
             call("Please note that this seed uses the dev build of the randomizer."),
             call("Download: https://github.com/tanjo3/wwrando/releases/tag/dev_tanjo3.1.10.7.3"),
+            call("Tracker: https://wooferzfg.me/tww-rando-tracker/s9-tournament"),
+        ])
+
+        self.assertEqual(mock_set_raceinfo.call_count, 1)
+        mock_set_raceinfo.assert_has_calls([
+            call(f"PERMA_{permalink} | Seed Hash: SEED HASH", False, False),
+        ])
+
+        self.assertEqual(mock_generate_seed.call_count, 1)
+        mock_generate_seed.assert_has_calls([
+            call(
+                randomizer_path=RandomizerPath.WWRANDO_DEV,
+                permalink=permalink,
+                prefix="test_user",
+                generate_spoiler_log=False,
+            )
+        ])
+
+    @patch.object(MockGenerator, "generate_seed", side_effect=mock_generate_seed_standard)
+    @patch.object(RandoHandler, "set_raceinfo", return_value=async_return(None))
+    @patch.object(RandoHandler, "send_message", return_value=async_return(None))
+    async def test_rolldevseed_other_permalink(self, mock_send_message, mock_set_raceinfo, mock_generate_seed):
+        generator = MockGenerator()
+        state = {}
+        handler = create_rando_handler(generator, state)
+        await handler.ex_rolldevseed(["miniblins-s3"], get_mock_message_data())
+
+        self.assertEqual(mock_send_message.call_count, 6)
+        permalink = "eJxLSS2LL0nMy8o31jPUMzTQM9czjk8xNkgxMk9mcGToNEhgYGAwZGGQcGAgEjggkWAWlzKYVmDqlPjRosDCwMDEyMSiICDEwLAArogDAA12DiI="  # noqa: E501
+        mock_send_message.assert_has_calls([
+            call("Rolling seed..."),
+            call(f"Permalink: PERMA_{permalink}"),
+            call("Seed Hash: SEED HASH"),
+            call("Please note that this seed uses the dev build of the randomizer."),
+            call("Download: https://github.com/tanjo3/wwrando/releases/tag/dev_tanjo3.1.10.7.3"),
             call("Tracker: https://wooferzfg.me/tww-rando-tracker/wwrando-dev-tanjo3"),
         ])
 
@@ -296,7 +331,7 @@ class TestHandler(unittest.IsolatedAsyncioTestCase):
             call("Seed Hash: SEED HASH"),
             call("Please note that this seed uses the dev build of the randomizer."),
             call("Download: https://github.com/tanjo3/wwrando/releases/tag/dev_tanjo3.1.10.7.3"),
-            call("Tracker: https://wooferzfg.me/tww-rando-tracker/miniblins"),
+            call("Tracker: https://wooferzfg.me/tww-rando-tracker/wwrando-dev-tanjo3"),
         ])
 
         self.assertEqual(mock_set_raceinfo.call_count, 1)
@@ -331,7 +366,7 @@ class TestHandler(unittest.IsolatedAsyncioTestCase):
             call("Seed Hash: SEED HASH"),
             call("Please note that this seed uses the dev build of the randomizer."),
             call("Download: https://github.com/tanjo3/wwrando/releases/tag/dev_tanjo3.1.10.7.3"),
-            call("Tracker: https://wooferzfg.me/tww-rando-tracker/wwrando-dev-tanjo3"),
+            call("Tracker: https://wooferzfg.me/tww-rando-tracker/s9-tournament"),
         ])
 
         self.assertEqual(mock_set_raceinfo.call_count, 1)
